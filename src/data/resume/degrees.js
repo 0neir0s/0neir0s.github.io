bfa66f35fed6be539699f65fb9278fb26b567b7e
0neir0s <athular7@gmail.com>
2021-06-02 15:34:16 +0530

    Corrected year type error

--- src/data/resume/degrees.js
+++ src/data/resume/degrees.js
@@ -3,12 +3,12 @@ const degrees = [
     school: 'WorldQuant University',
     degree: 'M.Sc. Financial Engineering',
     link: 'https://wqu.org',
-    year: '2018-20',
+    year: 2020,
   }, {
     school: 'Indian Institute of Technology, Bombay',
     degree: 'B.Tech(hons) Electrical Engineering with Minor in Computer Science',
     link: 'http://iitb.ac.in',
-    year: '2014-18',
+    year: 2018,
   },
 ];
 
