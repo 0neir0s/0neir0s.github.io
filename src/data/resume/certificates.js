9ebd3792101c0e9fc2970300b673c60f605de807
0neir0s <athular7@gmail.com>
2021-06-02 15:30:08 +0530

    Corrected year type error

--- src/data/resume/certificates.js
+++ src/data/resume/certificates.js
@@ -3,12 +3,12 @@ const certifications = [
     org: 'AWS',
     certification: 'AWS Certified Solutions Architect – Associate',
     link: 'https://aws.amazon.com',
-    year: '2020-23',
+    year: 2020,
   }, {
     org: 'Nutanix',
     certification: 'Hybrid Cloud Nanodegree',
     link: 'https://nutanix.com',
-    year: '2020-',
+    year: 2020,
   },
 ];
 
