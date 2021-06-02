c08c15b5029537c7510279ac07360c97c08004b0
0neir0s <athular7@gmail.com>
2021-06-02 13:58:34 +0530

    Adding link to one-page resume

--- src/pages/Index.js
+++ src/pages/Index.js
@@ -17,7 +17,7 @@ const Index = () => (
         </div>
       </header>
       <p>
-        Hi, I&apos;m Athul, an electrical engineer and a software developer. <br />
+        Hi, I&apos;m Athul, an electrical engineer and a software developer. (<a href="https://0neir0s.github.io/documents/resume.pdf" target="_blank" rel="noreferrer">One page resume</a>) <br />
         An <a href="https://www.iitb.ac.in/">IIT Bombay</a> and <a href="https://www.wqu.org/">WQU</a> graduate, I currently work as an Associate in <a href="https://www.jpmorganchase.com/">JP Morgan Chase &amp; Co</a>.<br />Feel free to check out my {' '}<Link to="/resume">resume</Link>, {' '}<Link to="/projects">projects</Link>, {' '}view <Link to="/stats">site statistics</Link>, {' '}or <Link to="/contact">contact</Link> me.<br />I also maintain a TiddlyWiki named <a href="https://0neir0s.github.io/kladde" target="_blank" rel="noreferrer">Kladde</a>.
       </p>
     </article>
